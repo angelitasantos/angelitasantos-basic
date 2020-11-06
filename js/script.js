91c97cff5ac2be9b8446dd73d7734d1fd2f67b5f
Angelita Santos <65367322+angelitasantos@users.noreply.github.com>
2020-11-06 08:08:54 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 // variáveis Header e Footer
 
 var banner_titulo = "NUNCA PARE DE APRENDER!"
-var rodape = "Deixe aqui seu e-mail que entrarei em contato."
+var rodape = "Clique na imagem."
 var copyright = "Copyright © Angelita Santos 2020. Todos os direitos reservados."
 
 $("#banner_titulo").html(banner_titulo);
